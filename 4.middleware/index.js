34ddfc4dd3ec1b81f4e51dc1ec5d42ed0eb924ad
shreyans <ssaklecha@primesoft.net>
2024-04-11 18:53:14 +0530

    modified

--- 4.middleware/index.js
+++ 4.middleware/index.js
@@ -2,17 +2,15 @@ const express = require('express');
 const app = express();
 
 
-const mid1 = (req,resp,next)=>{
+app.use((req,resp,next)=>{
     console.log("In the middleware");
     next();
-}
+});
 
-const mid2 = (req,resp,next)=>{
+app.use((req,resp,next)=>{
     console.log("In the another middleware");
     resp.send(`<h1> hello to node js </h1>`);
-}
-
-app.use(mid1,mid2);
+});
 
 app.get('',(req,resp)=>{
 
